c7e313ab2d568ddc85639af8ef3584dc92d13597
Mafe Avendaño <mfavendannom@correo.udistrital.edu.co>
2022-04-26 09:05:30 -0500

    ajustando funciones clave

--- desafio1_port_scanner.py
+++ desafio1_port_scanner.py
@@ -5,11 +5,28 @@
 A simple Python Port Scanner with Python: python-nmap y python3-nmap
 """
 
+import sys
+import json
+#import csv
 from datetime import datetime
 #import colorama
 from colorama import Fore #Back, Style
 
 timestamp = datetime.now()
+
+try:
+    import nmap
+    print("Versión del módulo: " + str(nmap.__version__))
+except ModuleNotFoundError:
+    print("Error import nmap, check your python-nmap instalation.")
+    sys.exit(-1)
+
+try:
+    import nmap3
+    print("Versión del módulo: " + str(nmap3.__version__)) #nmap3.nmap_version()
+except ModuleNotFoundError:
+    print("Error import nmap, check your python3-nmap instalation.")
+    sys.exit(-1)
 
 def write_csv(data):
     """
@@ -31,11 +48,58 @@ def escaner_nmap_tipo1():
     """
     Function escaner with python-nmap
     """
+    print("python-nmap")
+
+    target = input("Ingrese direccion IP o rango a escanear: ")
+    tipo_escaneo = input("Ingrese el tipo de escaneo: ej: -n -sV -sC -p ")
+    ports = input("Ingrese los puertos o rango a escanear: ")
+
+    nmap_arguments = tipo_escaneo + ports
+
+    scanner = nmap.PortScanner()
+    results = scanner.scan(hosts=target, arguments=nmap_arguments)
+    print(results)
+    print("Comando utilizado: " + scanner.command_line())
+
+    for host in scanner.all_hosts():
+        print("----------------------------------------------")
+        print('Host: ' + host + " (" + scanner[host].hostname() + ")")
+        print('Status: ' + scanner[host].state())
+
+        for protocol in scanner[host].all_protocols():
+            print("----------------------------------------------")
+            print('Protocol: ' + protocol)
+            ports_list = list(scanner[host][protocol].keys())
+            ports_list.sort()
+            for port in ports_list:
+                print(port)
+                print('Port: ' + str(port) + '\t Status: ' + scanner[host][protocol][port]['state'])
+
+    print("Exporting results to CSV file...")
+    write_csv(scanner.csv())
 
 def escaner_nmap3_tipo2():
     """
     Function escaner with python3-nmap
     """
+    print("python3-nmap")
+    target = input("Ingrese direccion IP o rango a escanear: ")
+    tipo_escaneo = input("Ingrese el tipo de escaneo: ej: -n -sV -sC -p ")
+    ports = input("Ingrese los puertos o rango a escanear: ")
+
+    print(target)
+    print(tipo_escaneo)
+    print(ports)
+    scanner = nmap3.Nmap()
+    results = scanner.scan_top_ports(target)
+
+    json_formatted = json.dumps(results, indent = 2)
+    print(json_formatted)
+
+    print("========= TOP PORTS ==========")
+    print(results)
+    print("==============================\n")
+    write_csv(json_formatted)
 
 def banner():
     """
@@ -54,7 +118,16 @@ def main():
     """
     Function main
     """
-    print(Fore.RED+"+"+Fore.RED+"-"*50+Fore.RED+"+")
+    print(Fore.RED+"+"+Fore.RED+"-"*50+Fore.RED+"+"+Fore.GREEN+"+")
+
+    opcion = input("Seleccione la opción a utilizar: \n 1. python-nmap \n 2. python3-nmap \n")
+    print(opcion)
+    if opcion == "1":
+        escaner_nmap_tipo1()
+    elif opcion == "2":
+        escaner_nmap3_tipo2()
+    else:
+        print("Ingrese una de las opciones del menú")
 
 banner()
 main()
